39607e12c8dac99ed489f31e63e42b42e5121abb
ser.korzhikov <ser.korzhikov@gmail.com>
2017-08-21 12:55:50 +0300

    upd

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/*importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 firebase.initializeApp({
@@ -6,6 +6,10 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onMessage(function(payload) {
+  console.log("Message received. ", payload);
+});
 
 messaging.setBackgroundMessageHandler(function(payload) {
 	console.log('message ', payload);
@@ -19,39 +23,6 @@ messaging.setBackgroundMessageHandler(function(payload) {
 	return self.registration.showNotification(notificationTitle,
 		notificationOptions);
 });
-*/
-self.addEventListener('push', function(event) {
 
-console.log(event);
 
-	var notification = event.data.json();
-
-	var title = notification.title;
-	var body = notification.body;
-	var icon = 'img/logo-eloplay.svg';
-	var tag = notification.tag;
-	var data = notification.data;
-
-	event.waitUntil(
-		self.registration.showNotification(title, {
-			body: body,
-			icon: icon,
-			tag: tag,
-			data: data
-		})
-	);
-});
-
-self.addEventListener('notificationclick', function(event) {
-//	console.log('On notification click: ', event);
-	if (Notification.prototype.hasOwnProperty('data')) {
-//		console.log('Using Data');
-		var url = event.notification.data.url;
-		if(url != undefined && url != ''){
-			event.waitUntil(clients.openWindow(url));
-		}
-	} else {
-		console.log('url not provided');
-	}
-});
 
