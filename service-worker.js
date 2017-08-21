86779bfdb060ddf11ef5091dc6f03d5ab09a1213
ser.korzhikov <ser.korzhikov@gmail.com>
2017-08-21 13:05:55 +0300

    upd

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/*
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
@@ -27,4 +28,51 @@ messaging.setBackgroundMessageHandler(function(payload) {
 });
 
 
+*/
+self.addEventListener('push', function(event) {
+
+		/*
+	event.waitUntil(
+		fetch('https://pubsub.googleapis.com/v1/projects//subscriptions/mysubscription:pull').then(function(response) {
+			console.log(response);
+		});
+		self.registration.showNotification(title, {
+			body: body,
+			icon: icon,
+			tag: tag,
+			data: data
+		})
+	);
+
+		*/
+	var notification = event.data.json();
+
+	var title = notification.title;
+	var body = notification.body;
+	var icon = 'img/logo-eloplay.svg';
+	var tag = notification.tag;
+	var data = notification.data;
+
+	event.waitUntil(
+		self.registration.showNotification(title, {
+			body: body,
+			icon: icon,
+			tag: tag,
+			data: data
+		})
+	);
+});
+
+self.addEventListener('notificationclick', function(event) {
+//	console.log('On notification click: ', event);
+	if (Notification.prototype.hasOwnProperty('data')) {
+//		console.log('Using Data');
+		var url = event.notification.data.url;
+		if(url != undefined && url != ''){
+			event.waitUntil(clients.openWindow(url));
+		}
+	} else {
+		console.log('url not provided');
+	}
+});
 
