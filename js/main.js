d31e4b8f4dc673ede505bf44b85f90d2de289027
Andrey <apashenko@gmail.com>
2017-08-31 01:35:03 +0300

    add time

---
+++ js/main.js
@@ -0,0 +1,93 @@
+	angular.module('app', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+	$(function() {
+
+		AOS.init();
+
+		$('.in-media').slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			dots: true,
+			appendArrows: $('.sl-class-arrow'),
+			appendDots: $('.sl-class-dots'),
+			prevArrow: '<i class="icon-arrow-left"></i>',
+			nextArrow: '<i class="icon-arrow-right"></i>',
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 2
+					}
+				}, {
+					breakpoint: 500,
+					settings: {
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+
+		$('.slick-command').slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			dots: false,
+			appendArrows: $('.command-class-arrow'),
+			//appendDots: $('.command-class-dots'),
+			prevArrow: '<i class="icon-arrow-left"></i>',
+			nextArrow: '<i class="icon-arrow-right"></i>',
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 3
+					}
+				}, {
+					breakpoint: 650,
+					settings: {
+						slidesToShow: 2
+					}
+				}, {
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+
+		$('a#ex1').on("click", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+					top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 800);
+		});
+
+
+		var dataStart = moment('2017-10-09 12:00:00Z').valueOf();//2017-10-09 12:00:00 UTC
+
+		var x = setInterval(function() {
+
+				var distance = dataStart - moment().valueOf();
+				var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+				
+				$("#days").text(days);
+				$("#hours").text(hours);
+				$("#minutes").text(minutes);
+				$("#seconds").text(seconds);
+
+				/*    if (distance < 0) {
+						clearInterval(x);
+						document.getElementById("demo").innerHTML = "EXPIRED";
+				}*/
+		}, 1000);
+
+	});
