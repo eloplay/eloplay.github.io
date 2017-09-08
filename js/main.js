5cbca6874e0df9d7f36dbe712fa9c8f2b3cb5d4d
ser.korzhikov <ser.korzhikov@gmail.com>
2017-09-08 16:13:25 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -175,6 +175,7 @@
 	});
 var video = document.querySelector('video');
 window.addEventListener('touchstart', function videoStart() {
+	video.allowFullscreen = false;
 	video.play();
 	// remove from the window and call the function we are removing
 	this.removeEventListener('touchstart', videoStart);
