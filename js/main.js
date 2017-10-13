1f7d9dd2e9b69264a469ed1a093a41542c09f511
ser.korzhikov <ser.korzhikov@gmail.com>
2017-10-13 11:54:28 +0300

    up screen after start tokensale

--- js/main.js
+++ js/main.js
@@ -16,6 +16,18 @@
 
 	$(function() {
 
+        var walletaddr  = '0x5C086Fee7660ee643Dc07C7C436B9521D5D18dbF';
+        var network     = 'rinkeby';
+
+//            var walletaddr  = '0xe57dCf42Ed7C4A53a0dE0D3d7BEBb676D1ED4438';
+//            var network     = 'ropsten';
+
+        var infuraApiKey = 'U1fp9zS1uWSGbaXgOfH5';
+        var usdethRate  = 300;
+        var usdCap 		= 12000000;
+        var ethCap      = parseInt(usdCap / usdethRate);
+
+
 		AOS.init({
       //disable: window.innerWidth < 1024
       disable: true
@@ -75,7 +87,7 @@
 						slidesToShow: 2
 					}
 				}, {
-					breakpoint: 576,					
+					breakpoint: 576,
 					settings: {
 						verticalSwiping: true,
 						vertical: true,
@@ -108,7 +120,7 @@
 						slidesToShow: 2
 					}
 				}, {
-					breakpoint: 576,					
+					breakpoint: 576,
 					settings: {
 						verticalSwiping: true,
 						vertical: true,
@@ -188,31 +200,169 @@
 			$('body,html').animate({scrollTop: top}, 800);
 		});
 
-		var dataStart = moment('2017-10-16 12:00:00Z').valueOf();//2017-10-09 12:00:00 UTC
+        function number_format( number, decimals, dec_point, thousands_sep ) {	// Format a number with grouped thousands
+            //
+            // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+            // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+            // +	 bugfix by: Michael White (http://crestidg.com)
+            var i, j, kw, kd, km;
+            // input sanitation & defaults
+            if( isNaN(decimals = Math.abs(decimals)) ){
+            	decimals = 2;
+            }
+            if( dec_point == undefined ){
+            	dec_point = ",";
+            }
+            if( thousands_sep == undefined ){
+            	thousands_sep = ".";
+            }
+            i = parseInt(number = (+number || 0).toFixed(decimals)) + "";
+            if( (j = i.length) > 3 ){
+            	j = j % 3;
+            } else {
+            	j = 0;
+            }
+            km = (j ? i.substr(0, j) + thousands_sep : "");
+            kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+            //kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).slice(2) : "");
+            kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).replace(/-/, 0).slice(2) : "");
+            return km + kw + kd;
+        }
 
-		var x = setInterval(function() {
+        var dataStart = moment('2017-10-16 12:00:00Z').valueOf();//2017-10-16 12:00:00 UTC
+        var dataHour = dataStart + 3600 * 1000;
+        var dataDay = dataStart + 3600 * 24 * 1000;
+        var dataWeek = dataStart + 3600 * 24 * 7 * 1000;
+        var dataWeek2 = dataStart + 3600 * 24 * 7 * 2 * 1000;
+        var dataEnd = moment('2017-11-15 12:00:00Z').valueOf();//2017-11-15 12:00:00 UTC
+        var finished_by_cap = false;
 
-				var distance = dataStart - moment().valueOf();
-				var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-				var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-				
-				
-				days = days > 9 ? days : ( '0' + days);
-				hours = hours > 9 ? hours : ( '0' + hours);
-				minutes = minutes > 9 ? minutes : ( '0' + minutes);
-				seconds = seconds > 9 ? seconds : ( '0' + seconds);
-				$("#days").text(days);
-				$("#hours").text(hours);
-				$("#minutes").text(minutes);
-				$("#seconds").text(seconds);
+        function get_current_timestamp() {
+            return new moment().valueOf();
+        }
 
-				/*    if (distance < 0) {
-						clearInterval(x);
-						document.getElementById("demo").innerHTML = "EXPIRED";
-				}*/
-		}, 1000);
+        var toStart = dataStart - get_current_timestamp(),
+            firstHour = dataHour - get_current_timestamp(),
+            firstDay = dataDay - get_current_timestamp(),
+            firstWeek = dataWeek - get_current_timestamp(),
+            secondWeek = dataWeek2 - get_current_timestamp(),
+            toEnd = dataEnd - get_current_timestamp();
+
+        // ICO did not started yet
+		if(toStart > 0){
+			$('#main-screen').show();
+			setInterval(function() {
+				var distance = dataStart - get_current_timestamp();
+				if (distance > 0) {
+					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+					days = days > 9 ? days : ( '0' + days);
+					hours = hours > 9 ? hours : ( '0' + hours);
+					minutes = minutes > 9 ? minutes : ( '0' + minutes);
+					seconds = seconds > 9 ? seconds : ( '0' + seconds);
+					$("#days_pre").text(days);
+					$("#hours_pre").text(hours);
+					$("#minutes_pre").text(minutes);
+					$("#seconds_pre").text(seconds);
+				}else{
+					window.location.reload(true);
+				}
+			}, 1000);
+		}
+
+        // ICO ongoing
+		if(toStart <= 0 && toEnd > 0){
+			$('#sale-started').show();
+
+            if (firstHour >= 0) {
+                var currentCounter = dataHour;
+                $(".box-double").removeClass('hidden');
+                $("#first_hour_info").removeClass('hidden');
+                $("#first_hour_info_post").removeClass('hidden');
+            } else if (firstDay >= 0) {
+                var currentCounter = dataDay;
+                $(".box-double").removeClass('hidden');
+                $("#first_day_info").removeClass('hidden');
+                $("#first_day_info_post").removeClass('hidden');
+            } else if (firstWeek >= 0) {
+                var currentCounter = dataWeek;
+                $(".box-double").removeClass('hidden');
+                $("#first_week_info").removeClass('hidden');
+                $("#first_week_info_post").removeClass('hidden');
+            } else if (secondWeek >= 0) {
+                var currentCounter = dataWeek2;
+                $(".box-double").removeClass('hidden');
+                $("#second_week_info").removeClass('hidden');
+                $("#second_week_info_post").removeClass('hidden');
+            } else {
+                var currentCounter = dataEnd;
+                $(".box-time").removeClass('hidden');
+            }
+
+			setInterval(function() {
+				var distance = currentCounter - get_current_timestamp();
+				if (distance > 0) {
+					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+					days = days > 9 ? days : ( '0' + days);
+					hours = hours > 9 ? hours : ( '0' + hours);
+					minutes = minutes > 9 ? minutes : ( '0' + minutes);
+					seconds = seconds > 9 ? seconds : ( '0' + seconds);
+					$(".timer-days").text(days);
+					$(".timer-hours").text(hours);
+					$(".timer-minutes").text(minutes);
+					$(".timer-seconds").text(seconds);
+
+				}else{
+					window.location.reload(true);
+				}
+			}, 1000);
+		}
+
+        if(toStart <= 0){
+            update_ico_progress();
+        }
+
+        function update_ico_progress() {
+
+            var jsonUrl = "https://" + network + ".infura.io/" + infuraApiKey;
+            var postData = {
+                "jsonrpc": "2.0",
+                "id": 1,
+                "method": "eth_getBalance",
+                "params": [walletaddr,"latest"]
+            };
+
+            $.post( jsonUrl, JSON.stringify(postData),
+                function( data, error ) {
+                    if (data.result !== undefined) {
+                        var weiInvested = parseInt(data.result, 16);
+                        var ethInvested = Math.round((weiInvested / 1000000000000000000) * 100) / 100;
+                        var usdInvested = ethInvested * usdethRate;
+                        var prcInvested = Math.round((usdInvested / usdCap) * 100 * 100) / 100;
+
+                        $('#usd_invested').html(number_format(usdInvested, 0, '.', ',') + ' USD');
+                        $('#eth_invested').html(number_format(ethInvested, 2, '.', ',') + ' ETH');
+                        $('#prc_invested').css('width', prcInvested + '%');
+                        $('#usd_cap').html(number_format(usdCap, 0, '.', ',') + ' USD');
+                        $('#eth_cap').html(number_format(ethCap, 0, '.', ',') + ' ETH');
+                        $('.box-progressbar').removeClass('hidden');
+
+                        if (ethInvested >= ethCap) {
+                            finished_by_cap = true;
+                        }
+                        if (toEnd < 0 && !finished_by_cap) {
+                            setTimeout(function(){ update_ico_progress() }, 15000);
+                        }
+                    }
+                }
+            , 'json');
+        }
 
 		function play_video(){
 			$('video').each(function(){
